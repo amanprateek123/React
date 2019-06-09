fc99828dad3c3255753f7bc5db34f878ad4fa05f
Aman Prateek <amanpra333@gmail.com>
2019-06-09 20:29:49 +0530

    Two way Binding

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -12,18 +12,27 @@ class App extends Component {
  switchHandler = () => {
 this.setState({
   persons: [
-    {name:"Rishu", age:19},
+    {name: "Rishu", age:19},
     {name:"Laddu",age:18}
   ]
 })
+ }
+
+ changeHandler = (event) => {
+  this.setState({
+    persons: [
+      {name: event.target.value, age:19},
+      {name:"Laddu",age:18}
+    ]
+  })
  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am React App!</h1>
         <p>It is working!!</p>
-        <button onClick={this.switchHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies:Coding</Person>
+        <button onClick={() => this.switchHandler()}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchHandler} changed={this.changeHandler}>My Hobbies:Coding</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
       </div>
     );
--- myapp/src/Person/Person.js
+++ myapp/src/Person/Person.js
@@ -4,8 +4,9 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-             <p>I am {props.name} and {props.age} year old!</p>
+             <p onClick={props.click}>I am {props.name} and {props.age} year old!</p>
              <p><b>{props.children}</b></p>
+             <input type='text' onChange={props.changed} />
         </div>
        
     )
